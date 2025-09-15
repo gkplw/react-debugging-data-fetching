53d6b8f47e13f777e15d7dd3ff10a22b436813cc
Ponlawat <p.sangtub@gmail.com>
2025-09-15 15:52:34 +0700

    fix: update data fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get("https://dummyjson.com/products");
-        setProducts(response.data.data);
+        setProducts(response.data.products);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
